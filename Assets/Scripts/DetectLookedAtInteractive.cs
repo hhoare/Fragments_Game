ffb3209fa5545e9bfbc922e4940ce950443f499a
Henry Hoare <Henry.Hoare@iam.local>
2019-02-07 17:25:00 -0600

    Begins work on interactive objects
    adds DetectLookedAtInteractive script that currently raycasts a red debug line and prints to console the name of what its looking it
    
    This commit also detects imaginary changes with the flooded grounds asset

---
+++ Assets/Scripts/DetectLookedAtInteractive.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Detects interactive elements the player is looking at using a raycast
+/// https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
+/// </summary>
+
+
+public class DetectLookedAtInteractive : MonoBehaviour
+{
+    [Tooltip("Starting point of raycast used to detect interactable objects")]
+    [SerializeField]
+    private Transform raycastOrigin;
+
+    [Tooltip("How far from the raycastOrigin we will search for interactable objects")]
+    [SerializeField]
+    private float maxRange = 2.5f;
+    
+
+
+    private void FixedUpdate()
+    {
+        Debug.DrawRay(raycastOrigin.position, raycastOrigin.forward * maxRange, Color.red);
+        RaycastHit hitInfo;
+        bool objectWasDetected = Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hitInfo, maxRange);
+
+        if (objectWasDetected) {
+            Debug.Log($"Player is looking at {hitInfo.collider.gameObject.name}");
+        }
+
+    }
+
+
+}
