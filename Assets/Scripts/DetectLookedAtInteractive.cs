3e0843af10335fc987bf4aef12d7afeb47b3c5cd
Henry Hoare <Henry.Hoare@iam.local>
2019-02-07 18:07:15 -0600

    continues work on interactive objects

--- Assets/Scripts/DetectLookedAtInteractive.cs
+++ Assets/Scripts/DetectLookedAtInteractive.cs
@@ -10,15 +10,15 @@ using UnityEngine;
 
 public class DetectLookedAtInteractive : MonoBehaviour
 {
-    [Tooltip("Starting point of raycast used to detect interactable objects")]
+    [Tooltip("Starting point of raycast used to detect interactive objects")]
     [SerializeField]
     private Transform raycastOrigin;
 
-    [Tooltip("How far from the raycastOrigin we will search for interactable objects")]
+    [Tooltip("How far from the raycastOrigin we will search for interactive objects")]
     [SerializeField]
     private float maxRange = 2.5f;
-    
 
+    public IInteractive lookedAtInteractive;
 
     private void FixedUpdate()
     {
@@ -26,9 +26,21 @@ public class DetectLookedAtInteractive : MonoBehaviour
         RaycastHit hitInfo;
         bool objectWasDetected = Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hitInfo, maxRange);
 
+
+        IInteractive interactive = null;
+
         if (objectWasDetected) {
-            Debug.Log($"Player is looking at {hitInfo.collider.gameObject.name}");
+            //Debug.Log($"Player is looking at {hitInfo.collider.gameObject.name}");
+
+            interactive = hitInfo.collider.gameObject.GetComponent<IInteractive>();
         }
+
+        if (interactive != null)
+        {
+            lookedAtInteractive = interactive;
+            //interactive.InteractWith();
+        }
+
 
     }
 
