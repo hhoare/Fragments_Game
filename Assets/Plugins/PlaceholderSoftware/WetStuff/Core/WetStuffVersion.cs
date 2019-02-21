1b9750d8e79b41d349c70855049ba4e2f7455a1e
hhoare <henry.hoare@hotmail.com>
2019-02-21 11:34:56 -0600

    SO MANY ASSETS and test level work
    start room and bathroom and hallway

---
+++ Assets/Plugins/PlaceholderSoftware/WetStuff/Core/WetStuffVersion.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace PlaceholderSoftware.WetStuff
+{
+    public partial class WetStuff
+        : MonoBehaviour
+    {
+        public static readonly SemanticVersion Version = new SemanticVersion(2, 0, 0, null);
+    }
+}
